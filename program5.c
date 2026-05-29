e4ee0cc4bde6cf91c63272258c85d2588aab8135
sam18singh <iamshambhawi0@gmail.com>
2026-05-29 19:26:37 +0530

    Add files via upload

---
+++ program5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int n,r,s=0;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        s=s+r;
+        n=n/10;
+        printf("sumofdigit=%d",s);    
+    }
+}
