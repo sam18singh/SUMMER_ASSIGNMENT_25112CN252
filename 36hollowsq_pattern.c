88c0d24b0b455721b43fc585b07fd274f026cf9a
sam18singh <iamshambhawi0@gmail.com>
2026-06-29 00:01:18 +0530

    Add files via upload

---
+++ 36hollowsq_pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int size=5;
+    for(int i=1;i<=size;i++)
+    {
+        for(int j=1;j<=size;j++)
+        {
+        if(i==1||i==size||j==1||j==size)
+        {
+          printf("*");  
+        }
+        else
+        {
+            printf(" ");
+        }
+    }
+    printf("\n");
+}
+}
