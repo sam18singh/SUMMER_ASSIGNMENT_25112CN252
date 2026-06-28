7cbbb69c2e6056465ffcac8f0b2ab12a7c3ecf45
sam18singh <iamshambhawi0@gmail.com>
2026-06-29 00:12:19 +0530

    Add files via upload

---
+++ 38rev_pyramid.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int rows=5;
+    for(int i=rows;i>=1;i--)
+    {
+        for(int j=1;j<=rows-i;j++)
+        {
+           printf(" ");
+        }
+        for(int j=1;j<=(2*i-1);j++)
+        {
+          printf("*");  
+        }
+    printf("\n");
+}
+}
